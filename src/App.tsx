c7d2f366fe5e93efbcab9bc29db58ff260901894
othsoh <sohabsouri@gmail.com>
2023-10-05 14:03:36 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex-col justify-between w-full h-full'>
+        <Navbar />
+        <div className='text-8xl'>App</div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+    theme: {
+    extend: {
+      colors: {
+        "black": "#263238",
+        "grey-10" : "#ABBED1",
+        "grey-20" : "#89939E",
+        "grey-30" : "#717171",
+        "grey-40" : "#4D4D4D",
+        "silver" : "#F5F7FA",
+        "white" : "#FFFFFF",
+        "green-10" : "#E8F5E9",
+        "green-20" : "#C8E6C9",
+        "green-30" : "#A5D6A7",
+        "green-40" : "#81C784",
+        "green-50" : "#66BB69",
+        "green-60" : "#43A046",
+        "green-70" : "#388E3B",
+        "green-80" : "#237D31",
+        "green-90" : "#1B5E1F",
+        "green-100" : "#103E13",
+        "warning" : "#FBC02D",
+        "error" : "#E53835",
+        "success" : "#2E7D31",
+        "primary" : "#28CB8B",
+        "secondary" : "#263238",
+        "info" : "#2194f3",
+      }
+    },
+  },
+  plugins: [],
+}
+
