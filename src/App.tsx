5699e499a58c3b804c0f8e42f44de6715eab0819
othsoh <sohabsouri@gmail.com>
2023-10-06 06:49:26 +0100

    t

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import MNavbar  from './components/navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Clients from './components/clients/clients'
 import Unlock from './components/unlock/unlock'
+import Achievements from './components/achievements/achievements'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <> 
         <Hero /> 
         <Clients />
+        <Achievements />
     </>
   )
 }
